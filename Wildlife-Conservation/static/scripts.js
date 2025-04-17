1642d8f3eb05122f8fcf89c5406d513056d54b7b
Rohit raj <126317686+rohit-raj7@users.noreply.github.com>
2025-04-17 19:59:18 +0530

    Delete Wildlife-Conservation directory

--- Wildlife-Conservation/static/scripts.js
+++
@@ -1,2 +0,0 @@
-import { debounce } from "@material-ui/core";
-
--- Wildlife-Conservation/user_models/user.py
+++
@@ -1,6 +0,0 @@
-from beanie import Document
-
-class User(Document):
-    username: str
-    hashed_password: str
-    is_admin: bool = False
